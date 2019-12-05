6c9ad9a4024c0d047eec05ec0d28857a5aa0b127
Abraham Maxfield <utabe11@hotmail.com>
2019-12-05 15:07:15 -0500

    GET for roles

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -7,3 +7,9 @@ class ContextTests(APITestCase):
     def test_get_contexts(self):
         response = self.client.get(reverse('context-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class RoleTests(APITestCase):
+    def test_get_roles(self):
+        response = self.client.get(reverse('role-list'))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
