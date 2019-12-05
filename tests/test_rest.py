5830167bbe63e8bef53799adb7df34b123eeb43e
Abraham Maxfield <utabe11@hotmail.com>
2019-12-05 16:11:58 -0500

    POST tests [WIP]

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -8,14 +8,26 @@ class ContextTests(APITestCase):
         response = self.client.get(reverse('context-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_post_(self):
+        response = self.client.post()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 
 class RoleTests(APITestCase):
     def test_get_roles(self):
         response = self.client.get(reverse('role-list'))
         self.assertEquals(response.status_code, status.HTTP_200_OK)
 
+    def test_post_(self):
+        response = self.client.post()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 
 class RoleAssignmentTests(APITestCase):
     def test_get_role_assignments(self):
         response = self.client.get(reverse('roleassignment-list'))
         self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+    def test_post_(self):
+        response = self.client.post()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
