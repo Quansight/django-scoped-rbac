88a96f9e438081c9808eb4af516496b063d78082
Abraham Maxfield <utabe11@hotmail.com>
2019-12-05 15:47:03 -0500

    RoleAssignment GET test

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -13,3 +13,9 @@ class RoleTests(APITestCase):
     def test_get_roles(self):
         response = self.client.get(reverse('role-list'))
         self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+
+class RoleAssignmentTests(APITestCase):
+    def test_get_role_assignments(self):
+        response = self.client.get(reverse('roleassignment-list'))
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
