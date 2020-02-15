bdfa0b0945945532f6c739378f0d333bfdbc7a84
David Charboneau <davidcharboneau@pop-os.localdomain>
2020-02-15 09:29:59 -0500

    Set the stage for SCRUD in the future.

--- tests/rest.py
+++ tests/rest.py
@@ -4,7 +4,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_condition import last_modified, condition
 from rest_framework_extensions.etag.decorators import etag
-from rest_framework_extensions.etag.mixins import ETAGMixin
 from scoped_rbac.rest import AccessControlledModelViewSet, DefaultPageNumberPagination
 from scoped_rbac.permissions import DEFAULT_CONTEXT, IsAuthorized
 from .models import ExampleRbacContext
--- tests/serializers.py
+++ tests/serializers.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from drf_envelopes.serializers import (
+from drf_scrud.serializers import (
         EnvelopeCollectionSerializer,
         EnvelopeItemSerializer,
         )
